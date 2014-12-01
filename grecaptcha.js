82fe39e27be6be12b6198705a61ed7ba22aa83ad
emorvillez <erwan.morvillez@orange.com>
2014-12-01 15:07:10 +0100

    add script and example

---
+++ grecaptcha.js
@@ -0,0 +1,75 @@
+'use strict';
+
+angular.module('grecaptcha', [])
+    .provider('grecaptcha', function() {
+        var _p = {},
+            self = this,
+            onloadMethod = 'onRecaptchaScriptLoaded';
+
+        this.setParameters = function(parameters) {
+            _p = parameters;
+        };
+
+        /**
+         * Add script for Google recaptcha
+         */
+        this._createScript = function($document) {
+            var scriptTag = $document.createElement('script');
+            scriptTag.type = 'text/javascript';
+            scriptTag.async = true;
+            scriptTag.defer = true;
+            scriptTag.src = '//www.google.com/recaptcha/api.js?onload=' + onloadMethod + '&render=explicit';
+            var s = $document.getElementsByTagName('body')[0];
+            s.appendChild(scriptTag);
+        };
+
+        this.$get = function($q, $window, $document) {
+            var deferred = $q.defer();
+            
+            // Define method called in global scope when recaptcha script is loaded.   
+            $window[onloadMethod] = function() {
+                deferred.resolve();
+            };
+            
+            if (!$window.grecaptcha) {
+                self._createScript($document[0]);
+            } else {
+                deferred.resolve();
+            }
+
+            return {
+                create: function(element, ngModelCtrl) {
+                    if (!_p || !_p.sitekey) {
+                        throw new Error('Please provide your sitekey via setParameters');
+                    }
+                    deferred.promise.then(function() {
+                        _p.callback = function(response) {
+                            // set the response value in ngModel
+                            ngModelCtrl.$setViewValue(response);
+                        };
+                        $window.grecaptcha.render(element, _p);
+                    });
+                },
+                destroy: function() {
+                    $window.grecaptcha.reset();
+                }
+            };
+        };
+
+    }).directive('grecaptcha', function(grecaptcha) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                ngModel: '='
+            },
+            link: function(scope, element, attrs, ngModelCtrl) {
+
+                // Create Element
+                grecaptcha.create(element[0], ngModelCtrl);
+
+                // Destroy Element
+                scope.$on('$destroy', grecaptcha.destroy);
+            }
+        };
+    });
