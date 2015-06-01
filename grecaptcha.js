1c1ff90401fac150e2b914ff86193aafe62bcf6e
Artem Berezin <zkhoden@gmail.com>
2015-06-01 18:53:13 +0900

    Lazy load api.js
    1. Load recaptcha api.js only when directive was used first time (not for every page reload)
    2. Reload api.js if language changed.

--- grecaptcha.js
+++ grecaptcha.js
@@ -29,20 +29,26 @@ angular.module('grecaptcha', [])
         };
 
         this.$get = ['$q', '$window', '$document', '$rootScope', function($q, $window, $document, $rootScope) {
-            var deferred = $q.defer();
-            
-            // Define method called in global scope when recaptcha script is loaded.   
-            $window[onloadMethod] = function() {
-                deferred.resolve();
-            };
-            
-            if (!$window.grecaptcha) {
-                self._createScript($document[0]);
-            } else {
-                deferred.resolve();
-            }
 
             return {
+                init: function() {
+                    if ($window.grecaptcha) {
+                        return $q.when();
+                    }
+                    var deferred = $q.defer();
+                    // Define method called in global scope when recaptcha script is loaded.
+                    $window[onloadMethod] = function() {
+                        deferred.resolve();
+                    };
+                    self._createScript($document[0]);
+                    return deferred.promise;
+                },
+                setLanguage: function(languageCode) {
+                    if (_l === languageCode) return;
+
+                    _l = languageCode;
+                    delete $window.grecaptcha; // force reload scripts with new language
+                },
                 updateParameters: function(parameters) {
                     angular.extend(_p, parameters);
                 },
@@ -50,16 +56,16 @@ angular.module('grecaptcha', [])
                     if (!_p || !_p.sitekey) {
                         throw new Error('Please provide your sitekey via setParameters');
                     }
+
                     function setValue(value) {
-                        $rootScope.$apply(function(){
+                        $rootScope.$apply(function() {
                             ngModelCtrl.$setViewValue(value);
                         });
                     }
-                    deferred.promise.then(function() {
-                        _p.callback = setValue;
-                        _p['expired-callback'] = setValue; // without arguments, value will be undefined
-                        $window.grecaptcha.render(element, _p);
-                    });
+
+                    _p.callback = setValue;
+                    _p['expired-callback'] = setValue; // without arguments, value will be undefined
+                    $window.grecaptcha.render(element, _p);
                 },
                 reset: function() {
                     $window.grecaptcha.reset();
@@ -67,7 +73,8 @@ angular.module('grecaptcha', [])
             };
         }];
 
-    }).directive('grecaptcha', ['grecaptcha', function(grecaptcha) {
+    })
+    .directive('grecaptcha', ['grecaptcha', function(grecaptcha) {
         return {
             restrict: 'A',
             require: 'ngModel',
@@ -76,11 +83,14 @@ angular.module('grecaptcha', [])
             },
             link: function(scope, element, attrs, ngModelCtrl) {
 
-                // Create Element
-                grecaptcha.create(element[0], ngModelCtrl);
+                grecaptcha.init()
+                    .then(function() {
+                        // Create Element
+                        grecaptcha.create(element[0], ngModelCtrl);
 
-                // Destroy Element
-                scope.$on('$destroy', grecaptcha.reset);
+                        // Destroy Element
+                        scope.$on('$destroy', grecaptcha.reset);
+                    });
             }
         };
     }]);
